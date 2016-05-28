19e050e5e46e2f58748fbcec1843d0c12432d68b
Dominick <dominickmalzone@aol.com>
2016-05-27 17:10:43 -0700

    skip signup if logged in

--- v1/weSplit/www/js/app.js
+++ v1/weSplit/www/js/app.js
@@ -1,44 +1,33 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngMessages','firebase','ngStorage'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $state, $localStorage) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
+
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
+    }
+
+    if($localStorage.user == ""){
+      console.log("No user logged in");
+      $state.go('signup');
     }
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
-
-  // Each tab has its own nav history stack:
 
       .state('login', {
       url: "/login",
@@ -92,7 +81,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','n
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('tab.dash');
 
 });
